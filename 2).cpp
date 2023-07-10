1126a863eea27d524f65520ea44663315797d460
Alangadeaa <102254187+Alangadeaa@users.noreply.github.com>
2023-07-10 18:24:06 -0300

    Update 2).cpp

--- 2).cpp
+++ 2).cpp
@@ -3,11 +3,10 @@
 int main() {
     int x, *y; // Declara una variable entera 'x' y un puntero a entero 'y'
     x = 10; // Asigna el valor 10 a la variable 'x'
-    y = &x; // El puntero 'y' apunta a la posición de memoria de 'x'
+    y = &x; // El puntero 'y' apunta a la posiciÃ³n de memoria de 'x'
 
     printf("%d %d %d", y, *y, &y);
-    // Imprime la posición de memoria de 'x', el valor almacenado en 'x'
-    // y la posición de memoria del puntero 'y'
+    // Imprime los resultados 
 
     return 0;
 }
